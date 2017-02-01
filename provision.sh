aeb8cb307b87b41f6e0be60dbb03434524e22f59
CameronRP <cameron.ryan.pears@gmail.com>
2017-02-02 12:52:09 +1300

    Updated to progresql 9.4

--- provision.sh
+++ provision.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+echo "Adding postgresql 9.4"
+sudo touch /etc/apt/sources.list.d/pgdg.list
+sudo echo 'deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main' > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
 echo "Update"
-#sudo apt-get update
+sudo apt-get update
+
 
 echo "Installing postgres"
-sudo apt-get install postgresql-9.3 -y
+sudo apt-get install postgresql-9.4 -y
 
-sudo cp /vagrant/pg_hba.conf /etc/postgresql/9.3/main/
-sudo cp /vagrant/postgresql.conf /etc/postgresql/9.3/main/
+sudo cp /vagrant/pg_hba.conf /etc/postgresql/9.4/main/
+sudo cp /vagrant/postgresql.conf /etc/postgresql/9.4/main/
 
 sudo chmod +x /vagrant/setup.sh
+
+echo "Adding new locale"
+sudo locale-gen en_NZ.UTF-8
 
 echo "Finishes provision"
